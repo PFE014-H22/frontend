0e381cc397e71cbacdb365a9da10168a1f6600f5
GRob27 <gab-roberge@hotmail.com>
2022-10-26 15:37:13 -0400

    fix: make storybook template smaller

--- src/components/percentage-bar/PercentageBar.stories.tsx
+++ src/components/percentage-bar/PercentageBar.stories.tsx
@@ -6,10 +6,13 @@ export default {
 	component: PercentageBar,
 } as ComponentMeta<typeof PercentageBar>;
 
-const Template: ComponentStory<typeof PercentageBar> = args => <PercentageBar {...args} />;
-
+const Template: ComponentStory<typeof PercentageBar> = args => (
+	<div className='max-w-xs'>
+		<PercentageBar {...args} />
+	</div>
+);
 export const Primary = Template.bind({});
 Primary.args = {
 	percentage: 0.5,
-	color: "bg-blue-500"
+	color: 'bg-blue-500',
 };
