8ab479e416ddccf0e29832d298577ab6edfe23f0
GRob27 <gab-roberge@hotmail.com>
2022-10-29 09:02:55 -0400

    feat: adding COLOR_SCHEME constant

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+/**
+ * Definition of the color scheme used inside the application
+ */
+export const COLOR_SCHEME = {
+	headerLinkSelectedColor: 'bg-[#c5c5c5]',
+	headerLinkTextColor: 'text-black',
+} as const;
