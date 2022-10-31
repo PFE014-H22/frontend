1cd035122ba6b764f1b0c84c9cdf3b93e0a761a2
GRob27 <gab-roberge@hotmail.com>
2022-10-31 15:57:16 -0400

    docs: add comments for clamp function

--- src/lib/clamp.ts
+++ src/lib/clamp.ts
@@ -1,3 +1,17 @@
-export const clamp = (num: number, min: number, max: number) => {
-	return Math.max(min, Math.min(num, max));
+/**
+ * Clamps a middle value within a range of values between a defined minimum bound and a maximum bound.
+ *
+ * @param preferredValue The number to clamp
+ * @param minimumBound The minimum acceptable value
+ * @param maximumBound The maximum acceptable value
+ * @returns The upper bound if the preferred number is higher than the maximum acceptable value.
+ * @returns The lower bound if the preferred number is lower than the minimum acceptable value.
+ * @returns The preferred value if in between the upper bound and the lower bound.
+ */
+export const clamp = (
+	preferredValue: number,
+	minimumBound: number,
+	maximumBound: number,
+) => {
+	return Math.max(minimumBound, Math.min(preferredValue, maximumBound));
 };
