d961753945d107a1cf0b79ae79c8f9177ee2addf
GRob27 <gab-roberge@hotmail.com>
2022-10-31 15:23:59 -0400

    docs: add comments to PercentageBar component and props

--- src/components/percentage-bar/PercentageBar.stories.tsx
+++ src/components/percentage-bar/PercentageBar.stories.tsx
@@ -6,10 +6,15 @@ export default {
 	component: PercentageBar,
 } as ComponentMeta<typeof PercentageBar>;
 
-const Template: ComponentStory<typeof PercentageBar> = args => <PercentageBar {...args} />;
+const Template: ComponentStory<typeof PercentageBar> = args => (
+	<div className='max-w-xs'>
+		<PercentageBar {...args} />
+	</div>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
+	backgroundColor: 'bg-[#B1B1B1]',
+	fillColor: 'bg-blue-500',
 	percentage: 0.5,
-	color: "bg-blue-500"
 };
--- src/components/percentage-bar/PercentageBar.tsx
+++ src/components/percentage-bar/PercentageBar.tsx
@@ -1,27 +1,48 @@
 import { clamp } from '../../lib/clamp';
 
+/**
+ * The props used to configure the percentage bar component.
+ */
 export interface PercentageBarProps {
+	/**
+	 * The background color of the percentage bar.
+	 * If provided, the color must either be a valid tailwind background color or a custom color in hexadecimal
+	 * You can read the tailwind docs (https://tailwindcss.com/docs/background-color) for more information.
+	 */
+	backgroundColor?:
+		| `bg-[#${string}]`
+		| `bg-${string}`
+		| `bg-${string}-${number}`;
+
+	/**
+	 * Color of the filling bar. Defaults to bg-blue-500
+	 * If provided, the color must either be a valid tailwind background color or a custom color in hexadecimal
+	 * You can read the tailwind docs (https://tailwindcss.com/docs/background-color) for more information.
+	 */
+	fillColor?: `bg-[#${string}]` | `bg-${string}` | `bg-${string}-${number}`;
+
 	/**
 	 * Percentage of the bar to fill between 0 and 1.
 	 */
 	percentage: number;
-
-	/**
-	 * Color of the filling bar. Defaults to bg-blue-500
-	 */
-	color?: string;
 }
 
+/**
+ * Component used to represent a percentage in a bar format.
+ */
 const PercentageBar = ({
+	backgroundColor,
+	fillColor = 'bg-blue-500',
 	percentage,
-	color = 'bg-blue-500',
 }: PercentageBarProps) => {
 	percentage = clamp(percentage, 0, 1);
 
 	return (
-		<div className="h-5 w-full max-w-full rounded-full bg-[#B1B1B1]">
+		<div
+			className={`h-5 w-full max-w-full rounded-full ${backgroundColor}`}
+		>
 			<div
-				className={`h-5 rounded-full ${color}`}
+				className={`h-5 rounded-full ${fillColor}`}
 				style={{ width: `${percentage * 100}%` }}
 			></div>
 		</div>
