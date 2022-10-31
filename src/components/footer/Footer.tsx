96513429409aefe4a6f02a4adf9e4aeab6d77705
GRob27 <gab-roberge@hotmail.com>
2022-10-31 15:59:42 -0400

    fix: make ets logo smaller

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ export interface FooterProps {}
 const Footer = ({}: FooterProps) => {
 	return (
 		<footer className="flex flex-row items-center justify-center gap-2">
-			<Image src="/Logo_ETS.jpg" width={64} height={64} />
+			<Image src="/Logo_ETS.jpg" width={48} height={48} />
 			<p className="text-2xl font-normal leading-7 text-[#6F6F6F]">
 				École de technologie supérieure, 2022
 			</p>
