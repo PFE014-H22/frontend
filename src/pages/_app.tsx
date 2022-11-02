9700b1cc90128d5f1c918e7444a7d1c19e3ae374
GRob27 <gab-roberge@hotmail.com>
2022-11-02 14:31:34 -0400

    create HomePage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppType } from 'next/app';
+import Footer from '../components/layout/footer/Footer';
+import Header from '../components/layout/header/Header';
 import { COLOR_SCHEME } from '../styles/colors';
 import '../styles/globals.css';
 import { trpc } from '../utils/trpc';
@@ -7,9 +9,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
 		<>
 			<div
-				className={`${COLOR_SCHEME.bodyBackgroundColor} mx-auto flex min-h-screen flex-col gap-2 p-4`}
+				className={`${COLOR_SCHEME.bodyBackgroundColor} flex min-h-screen flex-col gap-2 p-4`}
 			>
+				<Header />
 				<Component {...pageProps} />
+				<Footer />
 			</div>
 		</>
 	);
