3ef695741e642813eac72a34d19527b36e4020b0
GRob27 <gab-roberge@hotmail.com>
2022-11-02 22:10:09 -0400

    feat: implement SearchPage component

---
+++ src/components/pages/search/AnswerCard.tsx
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import { useMemo } from 'react';
+import { SearchResponse } from '../../../server/trpc/router/search';
+import Card from '../../common/card/Card';
+import PercentageMatchBar from '../../common/percentage-match-bar/PercentageMatchBar';
+import styles from './AnswerCard.module.scss';
+
+/**
+ * The props used to configure the AnswerCard component.
+ */
+export interface AnswerCardProps {
+	/**
+	 * Single answer object from the search backend response.
+	 */
+	answer: SearchResponse['answers'][0];
+
+	/**
+	 * The search term that was queried to get the answer.
+	 */
+	searchTerm: string;
+
+	/**
+	 * The technology that was selected to get the answer.
+	 */
+	technology: string;
+}
+
+const AnswerCard = ({ answer, searchTerm, technology }: AnswerCardProps) => {
+	const detailsHref = useMemo(() => {
+		let url = `/search/details/${answer.answer_id}`;
+		url += `?q=${encodeURIComponent(searchTerm)}`;
+		url += `&t=${encodeURIComponent(technology)}`;
+		return url;
+	}, [answer.answer_id, searchTerm, technology]);
+
+	return (
+		<Card>
+			<Link href={detailsHref} passHref>
+				<a className={styles.link__parameter}>Parameter</a>
+			</Link>
+
+			<div className={styles.content__container}>
+				<div className={styles.description__container}>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Mollitia beatae obcaecati omnis ut at quasi id
+						incidunt nam sit minus?
+					</p>
+				</div>
+
+				<div className={styles.parameter__results__container}>
+					<span className={styles.match__container}>xx matches</span>
+
+					<div className={styles.match__bar__container}>
+						<span className={styles.similarity__score} >
+							{(answer.similarity_score * 100).toFixed(2)}%
+						</span>
+
+						<PercentageMatchBar
+							percentage={answer.similarity_score}
+						/>
+					</div>
+				</div>
+			</div>
+		</Card>
+	);
+};
+
+export default AnswerCard;
