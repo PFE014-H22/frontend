a574773bea7d248ec6ee2c82e0fb6c675a336546
GRob27 <gab-roberge@hotmail.com>
2022-11-23 12:35:08 -0500

    lint: fix linter issues in storybook

--- src/components/pages/search/answer-card/AnswerCard.stories.tsx
+++ src/components/pages/search/answer-card/AnswerCard.stories.tsx
@@ -22,14 +22,13 @@ Primary.args = {
 		},
 		sources: [
 			{
-				answer_id: 8705014,
+				answer_id: '8705014',
 				question_body:
 					'<p>I tried to to this but it did not work, what am i doing wrong?</p>',
 				response_body:
 					'<p>Take a look at the following:</p>\n\n<p><a href="https://github.com/winglian/SSTable2S3" rel="nofollow">https://github.com/winglian/SSTable2S3</a></p>\n',
-				is_accepted: true,
 				link: 'https://stackoverflow.com/questions/8703370/does-any-way-to-automate-snaphot-processing-for-cassandra-on-the-aws/8705014#8705014',
-				name: 'stackoverflow' as const,
+				source_name: 'stackoverflow' as const,
 				question_id: '8703370',
 				question_title: 'How do i do this?',
 				similarity_score: 0.21544325589298968,
